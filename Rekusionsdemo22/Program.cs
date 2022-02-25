56233b1633fa0e9593569d19be4fa755a2be1c1a
Poul Henriksen <pohe@easj.dk>
2022-02-25 08:54:20 +0100

    Add project files.

---
+++ Rekusionsdemo22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Rekusionsdemo22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RekTester rek = new RekTester();
+
+            //rek.Happy(10);
+            rek.Happy2(10);
+
+
+            //samle resultatet op
+            int resultat = rek.Fib(8);
+
+            //Print resultatet
+            Console.WriteLine($"Resultatet er {resultat}");
+
+            string txt = "Regn in ger";
+            //kald og opsamling af resultatet
+            bool resul  = rek.Palindrom(txt.ToLower().Replace(" ", ""));
+
+            //udskriv om det er en palindrom
+            Console.WriteLine($"Er {txt} et palindrom {resul}");
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rekusionsdemo22/RekTester.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekusionsdemo22
+{
+    public class RekTester
+    {
+
+        //Iterativ udgave
+        public void Happy(int antal)
+        {
+            for (int i = 0; i < antal; i++)
+            {
+               Console.WriteLine("Jeg er lykkelig");
+            }
+        }
+
+
+        //Rekursiv udgave
+
+        public void Happy2(int antal)
+        {
+            if (antal>0)
+            {
+                Console.WriteLine("Jeg er lykkelig");
+                Happy2(antal-1);
+            }
+        }
+
+        //iterativ - lægge tallene sammen fra 0 til og med antal  
+        public int Sum(int antal)
+        {
+            int s = 0;
+            for (int i = 0; i <= antal; i++)
+            {
+                s += i;
+            }
+            return s;
+        }
+
+        //Rekursion - lægge tallene sammen fra 0 til og med antal 
+        public int SumRek(int antal)
+        {
+            if (antal >0)
+                return antal + SumRek(antal - 1);
+            else
+            {
+                return 0;
+            }
+        }
+
+        //Fakultet rekursivt
+
+        public int Fak(int antal)
+        {
+            if (antal > 0)
+                return antal * Fak(antal - 1);
+            else
+            {
+                return 1;
+            }
+        }
+
+        //Rekursiv fibonacci
+        public int Fib(int antal)
+        {
+            if (antal >=3)
+                return Fib(antal - 1) + Fib(antal - 2);
+            else
+                return 1;
+        }
+
+        //Returnerer om txt er en palindrom - rekursivt
+        public bool Palindrom(string txt)
+        {
+            if (txt.Length <= 1)
+                return true;
+            else if (txt[0] == txt[txt.Length - 1]) 
+                return Palindrom(txt.Substring(1, txt.Length - 2));
+            else
+                return false;
+        }
+
+    }
+}
